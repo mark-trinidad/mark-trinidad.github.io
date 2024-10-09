0e81e277e3100bf4b52857a850f733e3d864aa0a
Mark Trinidad <marktrinidad40@gmail.com>
2024-10-09 12:23:36 +0300

    navbar change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <main>
         <About />
         <Projects />
-
+        
       </main>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
-          <li><a href="#skills">Skills</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
