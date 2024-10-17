1f92ba98571e371cec9c037fc2c35d50d7ad4a9f
Mark Trinidad <marktrinidad40@gmail.com>
2024-10-17 17:37:54 +0300

    improvements

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import FadeUp from './FadeUp';
 import '../styles/About.css';
+
 
 const About = () => {
 
   return (
     <div id='about' className='about'>
+      
       <div className="column-right-abt">
-        <h1>ABOUT</h1>
-        <p>
-        Hello! I'm Mark from Philippines, and I grew up on the humble island of Bahrain. From a young age, I’ve been 
-        fascinated by how computers work. I remember experimenting with VMware and Linux to explore 
-        ethical hacking, which sparked my passion for technology and led me to pursue software engineering.
-        </p>
-        <p>Here are some of the technologies I've been working with:</p>
-        <ul className='skills-list'>
-          <li>ReactJS</li>
-          <li>JavaScript</li>
-          <li>C#</li>
-          <li>Python</li>
-          <li>SQL</li>
-          <li>HTML/CSS</li>
-        </ul>
-        <p>Beyond my work in software development, I'm deeply interested in advancements in Artificial Intelligence. 
-          In my free time, I enjoy playing games with friends and staying up-to-date with tech innovations.
-        </p>
+        <FadeUp delay="0.3s">
+          <h1>ABOUT</h1>
+          <p>
+          Hello! I'm Mark from Philippines, and I grew up on the humble island of Bahrain. From a young age, I’ve been 
+          fascinated by how computers work. I remember experimenting with VMware and Linux to explore 
+          ethical hacking, which sparked my passion for technology and led me to pursue software engineering.
+          </p>
+        </FadeUp>
+
+        <FadeUp delay="0.6s">
+          <p>Here are some of the technologies I've been working with:</p>
+          
+          <ul className='skills-list'>
+            <li>ReactJS</li>
+            <li>JavaScript</li>
+            <li>C#</li>
+            <li>Python</li>
+            <li>SQL</li>
+            <li>HTML/CSS</li>
+          </ul>
+        </FadeUp>
+        <FadeUp delay="0.75s">
+          <p>Beyond my work in software development, I'm deeply interested in advancements in Artificial Intelligence. 
+            In my free time, I enjoy playing games with friends and staying up-to-date with tech innovations.
+          </p>
+        </FadeUp>
       </div>
-  
+    
       <div className="column-left-abt">
+        <FadeUp delay="0.3s">
         <img src='/assets/me2.jpeg' alt='mark' width={500}></img>
+        </FadeUp>
       </div>  
+        
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,67 @@
 import React from 'react';
+import FadeUp from './FadeUp';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LaunchIcon from '@mui/icons-material/Launch';
 import '../styles/Projects.css';
 
 const Projects = () => {
   return (
     <div id='projects' className='projects'>
       <div className="title-projects">
-        <h1>PROJECTS</h1>
+        <FadeUp>
+          <h1>PROJECTS</h1>
+        </FadeUp>  
       </div>
-
+      
       <div className="flex-container">
         <div className="item-container">
+          <FadeUp delay="0.2s">
+            <div className="line"></div>
+            <div className="item">Deep Reinforcement Learning to Walk</div>
+            <p className='p-projects'>An application created for <b>University of Bolton Project, </b> 
+            this lets you train an AI bot that gets smarter over time using reinforcement learning.
+            </p>
+            <div className='icons-container'>
+              <GitHubIcon
+                className="MuiSvgIcon-root"
+                onClick={() => window.open('https://github.com/mark-trinidad/Deep-Reinforcement-Learning-to-Walk', '_blank')}
+              />
+              <LaunchIcon
+                className="MuiSvgIcon-root"
+                onClick={() => window.open('https://marktrinidad.itch.io/deep-reinforcement-learning-to-walk', '_blank')}
+              />
+            </div>
+          </FadeUp>
+        </div>
+        
+        <div className="item-container">
+          <FadeUp delay="0.5s">
+          <div className="line"></div>
           <div className="item">Attendance Management System</div>
-          <div className="line"></div>
+          <p className='p-projects'>An <b> Agile Project </b> made for University of Bolton.<br/>
+          Our group developed an Attendance Management System to automate attendance tracking for organizations. This involved detailed planning and agile methodologies.
+          </p>
+          <div className='icons-container'>
+            <GitHubIcon
+              className="MuiSvgIcon-root"
+              onClick={() => window.open('https://github.com/mark-trinidad/Attendance-Management-System', '_blank')}
+            />
+          </div>
+          </FadeUp>
         </div>
         <div className="item-container">
+         <FadeUp delay="0.8s">
+          <div className="line"></div>
           <div className="item">Tournament Finder</div>
-          <div className="line"></div>
-        </div>
-        <div className="item-container">
-          <div className="item">More projects</div>
-          <div className="line"></div>
+          <p className='p-projects'>I created a automated script for joining all tournaments in repeat.gg using pyautogui and tkinter(GUI)<br/>
+          </p>
+          <div className='icons-container'>
+            <GitHubIcon
+              className="MuiSvgIcon-root"
+              onClick={() => window.open('https://github.com/mark-trinidad/Tournament-Finder', '_blank')}
+            />
+          </div>
+          </FadeUp>
         </div>
       </div>
     </div>
